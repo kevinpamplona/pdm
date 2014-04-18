44f402e2d45a9a0226f3531f76bfbbdff5f2ce4b
kevinhoyinlau <kevinhoyinlau@gmail.com>
2014-04-18 11:20:39 -0700

    Modified some tests
    Fixed the testing module for the stage rendering to accomodate the new
    'stage name' property. Also added some testing for saving to an already
    existant stage.

--- stage/tests.py
+++ stage/tests.py
@@ -17,7 +17,7 @@ class RenderTestCase(TestCase):
 
     def testRender(self):
         dat = '  #  \nS   E\n#####'
-        data = {'width': 5, 'height': 3, 'data': dat}
+        data = {'width': 5, 'height': 3, 'data': dat, 'name': 'This Stage', 'id': None}
 
         # Test results when logged in
         self.client.login(username='login', password='login')
@@ -28,6 +28,8 @@ class RenderTestCase(TestCase):
         self.assertEqual(stage.width, 5)
         self.assertEqual(stage.height, 3)
         self.assertEqual(stage.owner, 'login')
+        self.assertEqual(stage.name, 'This Stage')
+        self.assertEqual(stage.pk, Stage.objects.count())
         self.assertTrue(len(Stage.objects.filter(owner='login')) > 0)
         self.client.logout()
 
@@ -39,6 +41,21 @@ class RenderTestCase(TestCase):
         self.assertEqual(stage.width, 5)
         self.assertEqual(stage.height, 3)
         self.assertEqual(stage.owner, '')
+        self.assertEqual(stage.name, 'This Stage')
+        self.assertEqual(stage.pk, Stage.objects.count())
+
+        # Test saving to an already-existing stage
+        data['id'] = Stage.objects.count()
+        data['data'] += '\n ### '
+        data['height'] += 1
+        response = self.client.post('/stage/render', data = json.dumps(data), content_type="application/json")
+        stageID = json.loads(response.content)['stageid']
+        self.assertEqual(stageID, data['id'])
+        stage = Stage.objects.get(pk=stageID)
+        self.assertNotEqual(stage.data, dat)
+        self.assertEqual(stage.height, 4)
+        self.assertEqual(stage.owner, '')
+        self.assertEqual(stage.name, 'This Stage')
 
     def tearDown(self):
         TestCase.tearDown(self)
