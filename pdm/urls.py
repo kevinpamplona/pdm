20ec25fca5dc4cd1075e2cfc0fde97d66df573ac
Kevin Pamplona <kevinpamplona@gmail.com>
2014-04-26 19:48:54 -0700

    search bar, user profiles, minor styles

--- pdm/urls.py
+++ pdm/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
 
     url(r'^login/', include('users.urls', namespace = 'users')),
     url(r'^search/$', 'users.views.search', name = 'search'),
+    url(r'^profile/$', 'users.views.profile', name = 'profile'),
 
     url(r'^stage/', include('stage.urls', namespace = 'stage')),
     url(r'^assets/', 'static_pages.views.static_asset'),
